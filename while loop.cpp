958b3ab6efb71ced37cbeafdb18653b446dfc408
Mohib565 <mohibshah565@gmail.com>
2025-04-29 22:42:36 +0500

    Add files via upload

---
+++ while loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int khoula= 0  ;
+    while (khoula<19)
+    
+    {cout << "khoula ghum" << endl;
+    khoula++ ;}
+    
+    return 0;
+}
